4f1cfd10adeebe08f268716e636b3500c871f289
Arun1only1 <arun1nly1@gmail.com>
2024-02-27 14:16:52 +0545

    add product

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import connectDB from "./connect.db.js";
+import productRoutes from "./product/product.routes.js";
+
+const app = express();
+// to make app understand json
+app.use(express.json());
+
+// ==================database connection=================================
+connectDB();
+
+// ==================register routes=================================
+app.use(productRoutes);
+
+// ==================port and server=================================
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
